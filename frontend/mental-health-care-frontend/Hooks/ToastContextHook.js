11d2f4db217beeaf5320d5784813c742aad94a7e
Thaiebu <thaiebu785@gmail.com>
2024-12-25 21:23:22 +0530

    frontend deleted

--- frontend/mental-health-care-frontend/Hooks/ToastContextHook.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { ToastContext } from "../stores/ToastContext";
-
-// Custom hook to use the ToastContext
-export const useToastContext = () => {
-  return useContext(ToastContext);
-};
--- frontend/mental-health-care-frontend/config/app_config.js
+++
@@ -1,2 +0,0 @@
-export const NODE_APP_URL =
-  import.meta.env.VITE_NODE_APP_URL || "http://localhost:3000";
